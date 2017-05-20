ac7dc32e18e7de90333b7b713c50e1ca376c4b80
Erick Montañez <codeams@gmail.com>
2017-05-20 17:49:55 -0500

    Setup basic structure, add Login spa component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,42 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,
+  body,
+  #app {
+    height: 100%;
+    background: #242326;
+  }
+
+  a {
+    text-decoration: none !important;
+  }
+
+  .main {
+    background: #f4f4f4;
+  }
+
+  .container-fluid {
+    padding: 0;
+  }
+
+  .btn-primary {
+    border: none;
+    border-radius: 1px;
+    background-color: #88c655;
+  }
+
+  .btn-primary:hover {
+    background-color: #7aa65d;
+  }
 </style>
---
+++ src/spa/Login.vue
@@ -0,0 +1,77 @@
+<template lang='html'>
+  <div id='login' class='login'>
+    <div class='container'>
+      <div class='row justify-content-center'>
+        <div class='col-md-5'>
+          <div class='login--card text-center'>
+            <h2 class='login--title'>cheat<strong>sheet</strong></h2>
+            <form>
+              <div class='form-group'>
+                <label for='exampleInputEmail1'>Email address</label>
+                <input type='email' class='form-control' id='exampleInputEmail1'
+                  aria-describedby='emailHelp' placeholder='Enter email'>
+                <small id='emailHelp' class='form-text text-muted'>
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+
+              <div class='form-group'>
+                <label for='exampleInputPassword1'>Password</label>
+                <input type='password' class='form-control'
+                  id='exampleInputPassword1' placeholder='Password'>
+              </div>
+
+              <a class='btn btn-primary login--button' href='topic.php'>
+                Submit
+              </a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  .login {
+    height: 100%;
+
+    background-image: url('~assets/images/desk.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .login--card {
+    height: 400px;
+    margin-top: 35%;
+    padding: 25px 40px;
+
+    color: white;
+    border-radius: 5px;
+    background: rgba(36, 35, 38, .84);
+  }
+
+  .login--card form {
+    text-align: left;
+  }
+
+  .login--title {
+    line-height: 50px;
+  }
+
+  .login--button {
+    width: 100%;
+    margin-top: 20px;
+    padding: 15px 100px;
+  }
+</style>
